142bce1486fdc181e8a3e49233f0901fee31cf35
Saldyrova Nazym <90493492+nazazaaa@users.noreply.github.com>
2024-03-16 17:40:28 +0500

    Add files via upload

---
+++ zhon/exit.js
@@ -0,0 +1,9 @@
+// document.getElementById('profileButton').addEventListener('mouseover', function() {
+//   document.getElementById('exitButton').style.right = '-90px';
+//   document.getElementById('exitButton').style.display: 'block';
+// });
+
+// document.getElementById('profileButton').addEventListener('mouseout', function() {
+//   document.getElementById('exitButton').style.right = '0px';
+//   document.getElementById('exitButton').style.display: 'none';
+// });
