0937ee9e8b7300aea521589d12e5d4cfcca4a0f3
Saldyrova Nazym <90493492+nazazaaa@users.noreply.github.com>
2024-04-06 00:20:41 +0500

    Delete zhon/exit.js

--- zhon/exit.js
+++
@@ -1,9 +0,0 @@
-// document.getElementById('profileButton').addEventListener('mouseover', function() {
-//   document.getElementById('exitButton').style.right = '-90px';
-//   document.getElementById('exitButton').style.display: 'block';
-// });
-
-// document.getElementById('profileButton').addEventListener('mouseout', function() {
-//   document.getElementById('exitButton').style.right = '0px';
-//   document.getElementById('exitButton').style.display: 'none';
-// });
